cae3ea82507059cf9b4db53896373268ba413fc9
prabakargr <prabakar1993@gmail.com>
2018-06-19 17:09:39 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,28 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { AddmissionComponent } from './addmission/addmission.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    LandingpageComponent,
+    AddmissionComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
     {path:'login',component:LoginComponent},
-    {path:'',pathMatch:'full',redirectTo:'login'}
+    {path:'',pathMatch:'full',redirectTo:'login'},
+    {path:'landingpage',component:LandingpageComponent,
+    children:[
+      {path:"addmission",component:AddmissionComponent},
+      {path:'',pathMatch:'full',redirectTo:'addmission'}
+    ]
+    }
     ])
   ],
   providers: [],
