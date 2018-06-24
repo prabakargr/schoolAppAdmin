a7930efa871afe50087248659c1aad2da657ae5b
prabakargr <prabakar1993@gmail.com>
2018-06-24 12:53:07 +0530

    teachers list updated

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map'
-import { resource } from 'selenium-webdriver/http';
+
 
 @Injectable()
 export class DataService {
